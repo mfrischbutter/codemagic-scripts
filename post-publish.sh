b24779c8f9f29d16287a281d6b3881933ad7caf1
Michael Frischbutter <mail.frischbutter@gmail.com>
2023-01-13 11:34:27 +0100

    feat: add jira template

--- post-publish.sh
+++ post-publish.sh
@@ -22,25 +22,21 @@ set -x
 IOS_ARTIFACT_TYPE=".ipa"
 ANDROID_ARTIFACT_TYPE=".apk"
 
-IOS_ARTIFACT_URL=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .url')
-IOS_ARTIFACT_NAME=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .name')
-IOS_TYPE=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .type')
-IOS_BUNDLE=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .bundleId')
+IOS_ARTIFACT_URL=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .url')
+IOS_ARTIFACT_NAME=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .name')
+IOS_TYPE=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .type')
+IOS_BUNDLE=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .bundleId')
 
-ANDROID_ARTIFACT_URL=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .url')
-ANDROID_ARTIFACT_NAME=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .name')
-ANDROID_TYPE=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .type')
-ANDROID_BUNDLE=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .bundleId')
+ANDROID_ARTIFACT_URL=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .url')
+ANDROID_ARTIFACT_NAME=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .name')
+ANDROID_TYPE=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .type')
+ANDROID_BUNDLE=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$ANDROID_ARTIFACT_TYPE"'")) | .bundleId')
 
 
-VERSION_NAME=$(echo $FCI_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .versionName')
+VERSION_NAME=$(echo $CM_ARTIFACT_LINKS | jq -r '.[] | select(.name | endswith("'"$IOS_ARTIFACT_TYPE"'")) | .versionName')
 BUILD_VERSION=$(( ${BUILD_NUMBER} + 1))
 
 BUILD_DATE=$(date +"%Y-%m-%d %H:%M:%S")
-
-IOS_TEST_URL=$(echo "${IPA_URL}" | sed 's#/#\\/#g')
-
-ANDROID_TEST_URL=$(echo "${APK_URL}" | sed 's#/#\\/#g')
 
 # Get the commit hash from the commit that links to the epic f.e. 'PROJ-1234: increase version to 1.4.3'
 COMMIT_HASH=$(echo "${FCI_COMMIT}" | sed 's/^\(........\).*/\1/;q')
@@ -54,8 +50,8 @@ JIRA_ISSUE=$(git log --pretty=oneline -n 1 | grep -e '[A-Z]\+-[0-9]\+' -o)
 sed -i.bak "s/\$BUILD_DATE/$BUILD_DATE/" .templates/jira.json
 sed -i.bak "s/\$ANDROID_ARTIFACT_NAME/$ANDROID_ARTIFACT_NAME/" .templates/jira.json
 sed -i.bak "s/\$IOS_ARTIFACT_NAME/$IOS_ARTIFACT_NAME/" .templates/jira.json
-sed -i.bak "s/\$ANDROID_ARTIFACT_URL/$TEST_URL/" .templates/jira.json
-sed -i.bak "s/\$IOS_ARTIFACT_URL/$TEST_URL/" .templates/jira.json
+sed -i.bak "s/\$ANDROID_ARTIFACT_URL/$ANDROID_ARTIFACT_URL/" .templates/jira.json
+sed -i.bak "s/\$IOS_ARTIFACT_URL/$IOS_ARTIFACT_URL/" .templates/jira.json
 sed -i.bak "s/\$FCI_COMMIT/$COMMIT/" .templates/jira.json
 sed -i.bak "s/\$GIT_COMMIT_MESSAGE/$GIT_COMMIT_MESSAGE/" .templates/jira.json
 sed -i.bak "s/\$VERSION_NAME/$VERSION_NAME/" .templates/jira.json
